743d99bdbadf2f78d852b07cd771ed8a226c9b4b
minusfivethirds <ryan.nicholas.king@gmail.com>
2013-04-08 16:30:52 -0600

    Octopress!

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/minusfivethirds/"
+http_images_path = "/minusfivethirds/images"
+http_fonts_path = "/minusfivethirds/fonts"
+css_dir = "public/minusfivethirds/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
